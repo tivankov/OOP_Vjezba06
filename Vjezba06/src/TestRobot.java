07390818cbda259a84eada230e29a701dd9f8311
gretasubasic16 <greta.subasic16@gmail.com>
2023-04-24 17:33:05 +0200

    Ispravan commit prvog zadatka

--- Vjezba06/src/TestRobot.java
+++ Vjezba06/src/TestRobot.java
@@ -1,8 +1,32 @@
+import Zadatak01.Robot;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class TestRobot {
     public static void main(String[] args) {
-        String test = "gjflkjghgLETTEERRR";
-        System.out.println(test);
-        System.out.println(test.length());
-        System.out.println(test.charAt(3));
+//        String test = "gjflkjghgLETTEERRR";
+//        System.out.println(test);
+//        System.out.println(test.length());
+//        System.out.println(test.charAt(3));
+//        int pos = (int) (Math.random()*(test.length())-1);
+////        System.out.println("Position: " + pos + " ch: " + test.charAt(pos));
+//        Robot robi1 = new Robot();
+//        System.out.println(robi1);
+//        Robot robi2 = new Robot();
+//        System.out.println(robi2);
+//        System.out.println(robi1.compare(robi1,robi2));
+        ArrayList<Robot> robots = generateListOfRobots(7);
+        System.out.println(robots);
+//        Collections.sort(robots);
+
+    }
+
+    private static ArrayList<Robot> generateListOfRobots(int numRobots){
+        ArrayList<Robot> robots = new ArrayList<>(numRobots);
+        for (int k =0; k < numRobots; k++){
+            robots.add(new Robot());
+        }
+        return robots;
     }
 }
--- Vjezba06/src/Zadatak01/Robot.java
+++ Vjezba06/src/Zadatak01/Robot.java
@@ -10,20 +10,59 @@ public class Robot implements Comparator<Robot> {
     private static final int maxl = 15;
     private static final String alph = "abcdefghijklmnprstuvzxywabcdefghijklmnoprsxyw123456789";
 
-    public Robot(String name){
+    public Robot(){
         this.name = name;
         this.id = cntID++;
     }
 
     @Override
     public int compare(Robot o1, Robot o2) {
-        return 0;
+        if(o1.getName().length() > o2.getName().length()){
+            return 1;
+        }else if (o1.getName().length() < o2.getName().length()){
+            return -1;
+        }else {
+            return 0;
+        }
     }
 
     private String genrateName(){
         String name = "";
         int lng_chars = (int) Math.random()*(maxl-minl) + minl;
+        for (int k = 0; k< lng_chars; k++){
+            int pos = (int) Math.random()+(alph.length()-1);
+            name += alph.charAt(pos);
+        }
 
         return name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public static int getCntID() {
+        return cntID;
+    }
+
+    public static void setCntID(int cntID) {
+        Robot.cntID = cntID;
+    }
+
+    @Override
+    public String toString() {
+        return "name='" + name;
     }
 }
